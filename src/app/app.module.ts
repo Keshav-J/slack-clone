146a7a35f96b00aaeebd53cb9c4dca6371f02f69
unknown <jkeshav15@gmail.com>
2020-11-07 12:58:06 +0530

    base project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
