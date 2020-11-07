ef7e170b21985877f9adeab23da1f1a6fcc55055
GoWtHaM6599 <gowthamsuresh6599@gmail.com>
2020-11-07 19:33:44 +0530

    messaging-panel initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,26 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { SideNavModule } from './main/side-nav/side-nav.module';
 
 import { AppComponent } from './app.component';
+import { ChannelComponent } from './main/messaging-panel/channel-view/channel/channel.component';
+import { ChannelHeaderComponent } from './main/messaging-panel/channel-view/channel-header/channel-header.component';
+import { PrivateComponent } from './main/messaging-panel/private-view/private/private.component';
+
+import { ChatboxComponent } from './shared/chatbox/chatbox.component';
+import { MessageContainerComponent } from './shared/message-container/message-container.component';
+import { MessageComponent } from './shared/message/message.component';
+import { PrivateHeaderComponent } from './main/messaging-panel/private-view/private-header/private-header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChannelComponent,
+    ChannelHeaderComponent,
+    PrivateComponent,
+    PrivateHeaderComponent,
+    ChatboxComponent,
+    MessageContainerComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
