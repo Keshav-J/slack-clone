729fde284855b2cc124b5a7e06b79b4c320bc97f
unknown <jkeshav15@gmail.com>
2020-11-09 01:34:32 +0530

    aside stable release

--- src/app/core/chat.service.ts
+++ src/app/core/chat.service.ts
@@ -1,16 +1,23 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
 
-  constructor(private http: HttpClient) { }
-    
-    
+  users: User[];
 
-     
+  constructor(private http: HttpClient) {
+    this.users = [
+      new User(1, 'John', 'Doe', true, "assets/profile-pic/profile-pic1.png"),
+      new User(2, 'Austine', '', false, "assets/profile-pic/profile-pic2.png"),
+      new User(3, 'Samantha', 'Ruth', false, "assets/profile-pic/profile-pic3.png"),
+      new User(4, 'Lily', '', true, "assets/profile-pic/profile-pic4.png"),
+      new User(5, 'Joe', 'Tribiani', true, "assets/profile-pic/profile-pic5.png")
+    ];
+  }
 
   message=[
     {"id":1,"username":"John","msg":"hi there","profile-pic":"assets/profile-pic/profile-pic1.png","time":new Date()},
@@ -31,5 +38,9 @@ export class ChatService {
 
   getMessages() {
     return this.message;
+  }
+
+  getUsers() {
+    return this.users;
   }
 }
