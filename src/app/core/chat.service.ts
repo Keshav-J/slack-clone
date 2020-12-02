e257ec05db067655d0e95295849c09a09e95ace1
Keshav-J <jkeshav15@gmail.com>
2020-12-02 20:32:19 +0530

    renamed channel, group, user ids to be more readable
    for testing purpose

--- src/app/core/chat.service.ts
+++ src/app/core/chat.service.ts
@@ -18,32 +18,32 @@ export class ChatService {
 
   constructor() {
     this.users = {
-      D01ATS70CSU: new User('D01ATS70CSU', 'John', 'Doe', true, 'assets/profile-pic/profile-pic1.png', 9548756481, 'john123@gmail.com'),
-      D01CYM5H84V: new User('D01CYM5H84V', 'Austine', '', false, 'assets/profile-pic/profile-pic2.png', 8456217496, 'austine@gmail.com'),
-      D01AKH52RPT: new User('D01AKH52RPT', 'Samantha', 'Ruth', false, 'assets/profile-pic/profile-pic3.png', 9845621452, 'sam@gmail.com'),
-      D01CDRCRBCM: new User('D01CDRCRBCM', 'Lily', '', true, 'assets/profile-pic/profile-pic4.png', 9854562154, 'lily@gmail.com'),
-      D01DZ6HTU78: new User('D01DZ6HTU78', 'Joe', 'Tribiani', true, 'assets/profile-pic/profile-pic5.png', 9874563210, 'how_you_doin@gmail.com'),
-      D01D8AHT8NB: new User('D01D8AHT8NB', 'Gowtham', 'S', true, 'assets/profile-pic/profile-pic6.png', 9791898746, 'gowthamsuresh6599@gmail.com')
+      D01JOHNDOE0: new User('D01JOHNDOE0', 'John', 'Doe', true, 'assets/profile-pic/profile-pic1.png', 9548756481, 'john123@gmail.com'),
+      D01AUSTINE0: new User('D01AUSTINE0', 'Austine', '', false, 'assets/profile-pic/profile-pic2.png', 8456217496, 'austine@gmail.com'),
+      D01SAMANTHA: new User('D01SAMANTHA', 'Samantha', 'Ruth', false, 'assets/profile-pic/profile-pic3.png', 9845621452, 'sam@gmail.com'),
+      D01LILY0000: new User('D01LILY0000', 'Lily', '', true, 'assets/profile-pic/profile-pic4.png', 9854562154, 'lily@gmail.com'),
+      D01JOETRIBI: new User('D01JOETRIBI', 'Joe', 'Tribiani', true, 'assets/profile-pic/profile-pic5.png', 9874563210, 'how_you_doin@gmail.com'),
+      D01GOWTHAMS: new User('D01GOWTHAMS', 'Gowtham', 'S', true, 'assets/profile-pic/profile-pic6.png', 9791898746, 'gowthamsuresh6599@gmail.com')
     };
 
     this.channels = {
-      C019P707QBZ: new Channel('C019P707QBZ',
+      C01GENERAL0: new Channel('C01GENERAL0',
                         'general',
                         '',
                         'This is the one channel that will always include everyone. It’s a great spot for announcements and team-wide conversations.',
                         false,
                         new Date(2020, 8, 4),
-                        ['D01ATS70CSU', 'D01CYM5H84V', 'D01AKH52RPT', 'D01CDRCRBCM', 'D01DZ6HTU78', 'D01D8AHT8NB'],
+                        ['D01JOHNDOE0', 'D01AUSTINE0', 'D01SAMANTHA', 'D01LILY0000', 'D01JOETRIBI', 'D01GOWTHAMS'],
                         [
-                          new Message(1, 'D01ATS70CSU', 'Hi there!', new Date()),
-                          new Message(2, 'D01CYM5H84V', 'Hey how r u', new Date()),
-                          new Message(3, 'D01AKH52RPT', 'okay', new Date()),
-                          new Message(4, 'D01CDRCRBCM', 'thank you', new Date()),
-                          new Message(5, 'D01ATS70CSU', 'Here comes Joe...', new Date()),
-                          new Message(6, 'D01DZ6HTU78', 'How you doin', new Date()),
-                          new Message(7, 'D01CYM5H84V', '😊', new Date()),
-                          new Message(8, 'D01AKH52RPT', '😊', new Date()),
-                          new Message(9, 'D01CDRCRBCM', '😊', new Date()),
+                          new Message(1, 'D01JOHNDOE0', 'Hi there!', new Date()),
+                          new Message(2, 'D01AUSTINE0', 'Hey how r u', new Date()),
+                          new Message(3, 'D01SAMANTHA', 'okay', new Date()),
+                          new Message(4, 'D01LILY0000', 'thank you', new Date()),
+                          new Message(5, 'D01JOHNDOE0', 'Here comes Joe...', new Date()),
+                          new Message(6, 'D01JOETRIBI', 'How you doin', new Date()),
+                          new Message(7, 'D01AUSTINE0', '😊', new Date()),
+                          new Message(8, 'D01SAMANTHA', '😊', new Date()),
+                          new Message(9, 'D01LILY0000', '😊', new Date()),
                         ],
                         [],
                         [
@@ -53,17 +53,17 @@ export class ChatService {
                           new File(4, 'HTML docs', 'Tim Berners-Lee', 'Oct 20th at 08:10', 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png')
                         ]
                       ),
-      C01A43X5PK4: new Channel('C01A43X5PK4',
+      C01TRAINING: new Channel('C01TRAINING',
                         'training',
                         '',
                         'Channel to share training related information and documents.',
                         false,
                         new Date(2020, 8, 4),
-                        ['D01AKH52RPT', 'D01DZ6HTU78', 'D01D8AHT8NB'],
+                        ['D01SAMANTHA', 'D01JOETRIBI', 'D01GOWTHAMS'],
                         [
-                          new Message(1, 'D01AKH52RPT', 'Hey', new Date()),
-                          new Message(2, 'D01DZ6HTU78', 'How you doin', new Date()),
-                          new Message(3, 'D01AKH52RPT', '😊', new Date()),
+                          new Message(1, 'D01SAMANTHA', 'Hey', new Date()),
+                          new Message(2, 'D01JOETRIBI', 'How you doin', new Date()),
+                          new Message(3, 'D01SAMANTHA', '😊', new Date()),
                         ],
                         [],
                         [
@@ -71,18 +71,18 @@ export class ChatService {
                           new File(2, 'HTML docs', 'Tim Berners-Lee', 'Oct 20th at 08:10', 'https://www.w3.org/html/logo/downloads/HTML5_Logo_512.png')
                         ]
                       ),
-      G01E2PATFS9: new Channel('G01E2PATFS9',
+      C01BUGSLAYE: new Channel('C01BUGSLAYE',
                       'bug-slayers',
                       '',
                       'This is private group. Not a channel.',
                       true,
                       new Date(2020, 11, 3),
-                      ['D01ATS70CSU', 'D01AKH52RPT', 'D01DZ6HTU78', 'D01D8AHT8NB'],
+                      ['D01JOHNDOE0', 'D01SAMANTHA', 'D01JOETRIBI', 'D01GOWTHAMS'],
                       [
-                        new Message(1, 'D01ATS70CSU', 'Hi', new Date()),
-                        new Message(2, 'D01AKH52RPT', 'Hey', new Date()),
-                        new Message(3, 'D01DZ6HTU78', 'How you doin', new Date()),
-                        new Message(4, 'D01AKH52RPT', '😊', new Date()),
+                        new Message(1, 'D01JOHNDOE0', 'Hi', new Date()),
+                        new Message(2, 'D01SAMANTHA', 'Hey', new Date()),
+                        new Message(3, 'D01JOETRIBI', 'How you doin', new Date()),
+                        new Message(4, 'D01SAMANTHA', '😊', new Date()),
                       ],
                       [],
                       [
@@ -93,12 +93,12 @@ export class ChatService {
     };
 
     this.members = [
-      this.users.D01ATS70CSU,
-      this.users.D01CYM5H84V,
-      this.users.D01AKH52RPT,
-      this.users.D01CDRCRBCM,
-      this.users.D01DZ6HTU78,
-      this.users.D01D8AHT8NB
+      this.users.D01JOHNDOE0,
+      this.users.D01AUSTINE0,
+      this.users.D01SAMANTHA,
+      this.users.D01LILY0000,
+      this.users.D01JOETRIBI,
+      this.users.D01GOWTHAMS
     ];
 
     this.files = [
@@ -109,20 +109,20 @@ export class ChatService {
     ];
 
     this.messages = [
-      new Message(1, 'D01ATS70CSU', 'Hi there!', new Date()),
-      new Message(2, 'D01CYM5H84V', 'Hey how r u', new Date()),
-      new Message(3, 'D01AKH52RPT', 'okay', new Date()),
-      new Message(4, 'D01CDRCRBCM', 'thank you', new Date()),
-      new Message(5, 'D01ATS70CSU', 'Here comes Joe...', new Date()),
-      new Message(6, 'D01DZ6HTU78', 'How you doin', new Date()),
-      new Message(7, 'D01CYM5H84V', '😊', new Date()),
-      new Message(8, 'D01AKH52RPT', '😊', new Date()),
-      new Message(9, 'D01CDRCRBCM', '😊', new Date()),
+      new Message(1, 'D01JOHNDOE0', 'Hi there!', new Date()),
+      new Message(2, 'D01AUSTINE0', 'Hey how r u', new Date()),
+      new Message(3, 'D01SAMANTHA', 'okay', new Date()),
+      new Message(4, 'D01LILY0000', 'thank you', new Date()),
+      new Message(5, 'D01JOHNDOE0', 'Here comes Joe...', new Date()),
+      new Message(6, 'D01JOETRIBI', 'How you doin', new Date()),
+      new Message(7, 'D01AUSTINE0', '😊', new Date()),
+      new Message(8, 'D01SAMANTHA', '😊', new Date()),
+      new Message(9, 'D01LILY0000', '😊', new Date()),
     ];
   }
 
   addToCart(msg: string): void {
-    this.messages.push(new Message(this.messages.length + 1, 'D01D8AHT8NB', msg, new Date()));
+    this.messages.push(new Message(this.messages.length + 1, 'D01GOWTHAMS', msg, new Date()));
   }
 
   getChannels(): { [key: string]: Channel } {
